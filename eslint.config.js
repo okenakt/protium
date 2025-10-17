22a624c7bf3b13152c74400fa5f3cd176a88e1a2
okenakt <19400343+okenakt@users.noreply.github.com>
2025-10-17 13:34:51 +0900

    Clean up package config

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      globals: {
+        ...globals.node,
+        ...globals.es6,
+        ...globals.mocha,
+        NodeJS: "readonly",
+        Thenable: "readonly",
+      },
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+      "no-undef": "warn",
+    },
+  },
+];
